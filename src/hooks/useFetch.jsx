02b32ccfc462621ad5298d0381af49d6f805d71c
mohammad <mohammad.mgs.98@gmail.com>
2023-03-09 15:17:36 +0330

    d

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -3,31 +3,29 @@ import { useState, useEffect } from "react";
 
 export default function useFetch() {
   let params = useParams();
+  console.log("params: ", params);
 
-  const [course, setCourse] = useState(); // course page
-  const [isPending, setIsPending] = useState(true); // course page
-  const [error, setError] = useState(false); // course page
-
-  const [allCourse, setAllCourse] = useState([]); // all courses page
+  const [course, setCourse] = useState();
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
-    fetch("https://react-b2956-default-rtdb.firebaseio.com/courses.json")
-      .then((res) => res.json())
-      .then((data) => {
-        setAllCourse(data);
-        let findCourse = data.find((course) => {
-          return course.id === +params.courseID;
+      fetch("https://react-b2956-default-rtdb.firebaseio.com/courses.json")
+        .then((res) => res.json())
+        .then((data) => {
+          let findCourse = data.find((course) => {
+            return course.id === +params.courseID;
+          });
+          findCourse && setCourse(findCourse);
+          setIsPending(false);
+          setError(false);
+        })
+        .catch((err) => {
+          setError(true);
+          setIsPending(false);
         });
-        findCourse && setCourse(findCourse);
-        setIsPending(false);
-        setError(false);
-      })
-      .catch((err) => {
-        setError(true);
-        setIsPending(false);
-      });
   }, []);
   // }, [params.courseID]);
 
-  return [course, isPending, error, allCourse];
+  return [course, isPending, error];
 }
